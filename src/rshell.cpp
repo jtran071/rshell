13194a2b46b01eb1eac60218e1c5bd7abac021ee
Jacky Tran <jtran071@hammer.cs.ucr.edu>
2015-04-19 13:36:22 -0700

    added comment

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -42,24 +42,28 @@ int main()
 		getline(cin, input_cmd);
 		
 		//parse command input
-		char_separator<char> delim(" ;&|");
+		char_separator<char> delim(" \t", "#;&|");
 		tokenizer< char_separator<char> > mytok(input_cmd, delim);
 		for(auto it = mytok.begin(); it != mytok.end(); ++it)
 		{
 			parse_list.push_back(*it);
 		}
-	
+			
 		vector<string> cmd_line;
 		vector<char*> arg(parse_list.size() + 1);
 		
 		string exit_flag = parse_list.front();
-	
-		while(!(parse_list.empty()))
+		
+		//push only the commands, saving connectors in list
+		while(!(parse_list.empty()) && parse_list.front() != "#" && 
+				parse_list.front() != ";" && parse_list.front() != "&" && 
+				parse_list.front() != "|")
 		{
 			cmd_line.push_back(parse_list.front());
 			parse_list.pop_front();
 		}
-      
+		
+		
 		for(unsigned int i = 0; i != cmd_line.size(); ++i)  
 		{
 			arg[i] = &cmd_line[i][0];
@@ -77,35 +81,50 @@ int main()
 		//TODO: CHECK FOR COMMENTS # 
 		//CONNECTORS		
 		
-		int pid = fork();
-		//error with fork
-		if(pid < 0)
+		//create loop to check for connectors and subsequent arg
+		//use a list to check connectors
+		//check whether the command succeeded or failed
+		//do connector
+		//
+		
+		while(!(cmd_line.empty()))
 		{
-			perror("fork()");
-			exit(1);
-		}
-		//child process
-		else if(pid == 0)
-		{
-			if(-1 == execvp(arg[0], arg.data()))//insert arg
+			if(parse_list.front() == "#")
 			{
-				perror("execvp()");
-				_exit(1);
+				cmd_line.clear();
 			}
-			_exit(0);
-		}
-		//parent process
-		else if(pid > 0)
-		{
-			if(-1 == wait(0))
+			else{
+				cmd_line.clear();
+			}
+			int pid = fork();
+			//error with fork
+			if(pid < 0)
 			{
-				perror("wait()");
+				perror("fork()");
 				exit(1);
 			}
+			//child process
+			else if(pid == 0)
+			{
+				if(-1 == execvp(arg[0], arg.data()))//insert arg
+				{
+					perror("execvp()");
+					_exit(1);
+				}
+				_exit(0);
+			}
+			//parent process
+			else if(pid > 0)
+			{
+				if(-1 == wait(0))
+				{
+					perror("wait()");
+					exit(1);
+				}
 		
+			}
 		}
 		
-		//arg.clear();
 
 	}
 
