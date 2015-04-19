a2eda3845426592b52f811e24b4408e8d93e966d
Jacky Tran <jtran071@hammer.cs.ucr.edu>
2015-04-18 17:46:13 -0700

    added tokenizer for parse

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <string>
 #include <boost/tokenizer.hpp>
+#include <list>
 
 using namespace std;
 using namespace boost;
@@ -21,15 +22,23 @@ int main()
 	while(1)
 	{
 		string input_cmd;
+		list<string> parse_list;
 		
-		//FIXME: Doesn't output after fg
+		//FIXME: Doesn't output after fg, error check if host/user dne
 		//	
 		//outputs user login and host name along with command prompt
 		cout << getlogin() << "@" << hostname << "$ ";
 
 		//takes in commands
 		getline(cin, input_cmd);
-
+		
+		//parse command input
+		char_separator<char> delim(" ;&|");
+		tokenizer< char_separator<char> > mytok(input_cmd, delim);
+		for(auto it = mytok.begin(); it != mytok.end(); ++it)
+		{
+			parse_list.push_back(*it);
+		}
 		
 
 		//if exit entered, exit shell
