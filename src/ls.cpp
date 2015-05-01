9a35893f80ceb8714c09fa5231c4c45786152b86
Jacky Tran <jtran071@hammer.cs.ucr.edu>
2015-04-30 23:10:31 -0700

    now takes multiple flags/arg

--- src/ls.cpp
+++ src/ls.cpp
@@ -10,32 +10,46 @@
 #include <pwd.h>
 #include <grp.h>
 #include <string.h>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
 void parse_flags(const int argc, char* argv[], bool flag_a,
-	bool flag_l, bool flag_R)
+	bool flag_l, bool flag_R, queue<string> &q_dirs)
 {
 	unsigned count = argc;
 	for(int i = count - 1; i != 0; --i)
 	{
 		if(argv[i][0] == '-')
 		{
-			if (argv[i][1] == 'a')
+			if(argv[i][1] == '\0')
 			{
-				flag_a = true;
-				cout << "yes flag a" << endl;
+				cout << "Error: invalid flag" << endl;
+				exit(1);
 			}
-			else if(argv[i][1] == 'l')
+			for(int j = 1; argv[i][j] != '\0'; ++j)
 			{
-				flag_l = true;
-				cout << "yes flag l" << endl;
+				if (argv[i][j] == 'a')
+				{
+					flag_a = true;
+					cout << "yes flag a" << endl;
+				}
+				else if(argv[i][j] == 'l')
+				{
+					flag_l = true;
+					cout << "yes flag l" << endl;
+				}
+				else if(argv[i][j] == 'R')
+				{
+					flag_R = true;
+					cout << "yes flag R" << endl;
+				}
 			}
-			else if(argv[i][1] == 'R')
-			{
-				flag_R = true;
-				cout << "yes flag R" << endl;
-			}
+		}
+		else
+		{
+			q_dirs.push(argv[i]);
 		}
 	}
 }
@@ -53,33 +67,41 @@ int main(int argc, char* argv[])
 		bool flag_a = false;
 		bool flag_l = false;
 		bool flag_R = false;
-		
-		parse_flags(argc, argv, flag_a, flag_l, flag_R);
 
-		DIR *dirp;
-		if(NULL == (dirp = opendir(argv[2])))
+		queue<string> q_dirs;
+		
+		parse_flags(argc, argv, flag_a, flag_l, flag_R, q_dirs);
+		
+		while(!q_dirs.empty())
 		{
-			perror("opendir()");
-			exit(1);
+			DIR *dirp;
+			if(NULL == (dirp = opendir(q_dirs.front().c_str())))
+			{
+				perror("opendir()");
+				exit(1);
+			}
+			struct dirent *files;
+			errno = 0;
+			while(NULL != (files = readdir(dirp)))
+			{
+				cout << files->d_name << " ";
+			}
+			
+			q_dirs.pop();
+
+			if(errno != 0)
+			{
+				perror("readdir()");
+				exit(1);
+			}
+			cout << endl;
+			if(-1 == closedir(dirp))
+			{
+				perror("closedir()");
+				exit(1);
+			}
 		}
-		struct dirent *files;
-		errno = 0;
-		while(NULL != (files = readdir(dirp)))
-		{
-			cout << files->d_name << " ";
-		}
-		if(errno != 0)
-		{
-			perror("readdir()");
-			exit(1);
-		}
-		cout << endl;
-		if(-1 == closedir(dirp))
-		{
-			perror("closedir()");
-			exit(1);
-		}
-	}
+	}	
 	return 0;
 
 }
